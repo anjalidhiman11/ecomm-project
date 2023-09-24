0b91e5d7274b3e6520443d195f39021e9b6e6c7f
anjalidhiman113 <anjalidhiman113@gmail.com>
2023-09-24 20:53:22 +0530

    form for seller page

--- ecom-project/src/app/seller-auth/seller-auth.component.ts
+++ ecom-project/src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+  sellerSignUpForm! : FormGroup;
+
+  constructor( private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.sellerSignUpForm= this.fb.group({
+      name : [''],
+      password : [''],
+      email:['']
+    })
+  }
+
+  signUp(data:any){
+    console.warn(data)
+  }
 
 }
